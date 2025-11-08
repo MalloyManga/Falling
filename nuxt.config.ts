6be3a332fa5bb126e4f87d9e6570b0eb9c24dd60
Malloy <li3210361050@gmail.com>
2025-11-08 21:21:35 +0800

    fix: 修改了一些关于 GitHub actions 部署的文件

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   modules: ['@tresjs/nuxt'],
   ssr: false,
+  app: {
+    baseURL: '/Falling/',
+  },
   devtools: { enabled: true },
 })
